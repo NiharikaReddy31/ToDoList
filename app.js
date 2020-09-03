c71e00ed5b35428f89c1d2583cf3d126f31f83af
Velmula Niharika <velmulaniharika31@gmail.com>
2020-09-03 15:14:35 +0530

    Initial Todo List

---
+++ app.js
@@ -0,0 +1,34 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+
+const app = express();
+
+var items=["Coding"," Learning Web Development Course"];
+
+app.set("view engine","ejs");
+app.use(bodyParser.urlencoded({extended:true}));
+app.get("/",function(req,res){
+  var today = new Date();
+  var options = {
+    weekday:"long",
+    day:"numeric",
+    month:"long"
+  }
+  var day=today.toLocaleDateString("en-US",options);
+  res.render("list",{
+    kindOfDay:day,
+    newListItems:items
+  });
+});
+
+app.post("/",function(req,res){
+  var item = req.body.newTodoItem;
+  console.log(item);
+  items.push(item);
+  res.redirect("/")
+});
+
+
+app.listen(3000,function(){
+  console.log("The server is up and running!");
+});
